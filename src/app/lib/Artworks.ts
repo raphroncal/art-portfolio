6622622d54e387508c2520c261cb9e0eff77761c
Raphael Roncal <raph.roncal01@gmail.com>
2025-08-11 07:53:35 +0800

    [Add] Artworks

---
+++ src/app/lib/Artworks.ts
@@ -0,0 +1,95 @@
+import branding_1 from "@/assets/art/branding/branding_1.png";
+import branding_2 from "@/assets/art/branding/branding_2.png";
+import branding_3 from "@/assets/art/branding/branding_3.png";
+import branding_4 from "@/assets/art/branding/branding_advocacy_1.png";
+import branding_5 from "@/assets/art/branding/branding_advocacy_2.png";
+import branding_6 from "@/assets/art/branding/branding_advocacy_3.png";
+import branding_7 from "@/assets/art/branding/branding_advocacy_4.png";
+
+import charcoal_1 from "@/assets/art/charcoal/charcoal_1.jpg";
+import charcoal_2 from "@/assets/art/charcoal/charcoal_2.jpg";
+import charcoal_3 from "@/assets/art/charcoal/charcoal_3.jpg";
+import charcoal_4 from "@/assets/art/charcoal/charcoal_4.jpg";
+import charcoal_5 from "@/assets/art/charcoal/charcoal_5.jpg";
+import charcoal_6 from "@/assets/art/charcoal/charcoal_6.jpg";
+import charcoal_7 from "@/assets/art/charcoal/charcoal_7.jpg";
+import charcoal_8 from "@/assets/art/charcoal/charcoal_8.jpg";
+
+import commission_1 from "@/assets/art/commissions/commission_1.jpg";
+import commission_2 from "@/assets/art/commissions/commission_2.jpg";
+import commission_3 from "@/assets/art/commissions/commission_3.jpg";
+import commission_4 from "@/assets/art/commissions/commission_4.jpg";
+import commission_5 from "@/assets/art/commissions/commission_5.png";
+
+import exploration_1 from "@/assets/art/self-exploration/exploration_1.jpg";
+import exploration_2 from "@/assets/art/self-exploration/exploration_2.jpg";
+import exploration_3 from "@/assets/art/self-exploration/exploration_3.jpg";
+import exploration_4 from "@/assets/art/self-exploration/exploration_4.png";
+import exploration_5 from "@/assets/art/self-exploration/exploration_5.jpg";
+import exploration_6 from "@/assets/art/self-exploration/exploration_6.jpg";
+import exploration_7 from "@/assets/art/self-exploration/exploration_7.jpg";
+import exploration_8 from "@/assets/art/self-exploration/exploration_8.png";
+
+import exploration_9 from "@/assets/art/self-exploration/zine-1.png";
+import exploration_10 from "@/assets/art/self-exploration/zine-2.png";
+import exploration_11 from "@/assets/art/self-exploration/zine-3.png";
+import exploration_12 from "@/assets/art/self-exploration/zine-4.png";
+import exploration_13 from "@/assets/art/self-exploration/zine-5.png";
+import exploration_14 from "@/assets/art/self-exploration/zine-6.png";
+import exploration_15 from "@/assets/art/self-exploration/zine-7.png";
+import exploration_16 from "@/assets/art/self-exploration/zine-8.png";
+import exploration_17 from "@/assets/art/self-exploration/zine-9.png";
+import exploration_18 from "@/assets/art/self-exploration/zine-10.png";
+import exploration_19 from "@/assets/art/self-exploration/zine-11.png";
+import exploration_20 from "@/assets/art/self-exploration/zine-11.png";
+
+export const artworks = {
+  branding: [
+    branding_1,
+    branding_2,
+    branding_3,
+    branding_4,
+    branding_5,
+    branding_6,
+    branding_7,
+  ],
+  charcoal: [
+    charcoal_1,
+    charcoal_2,
+    charcoal_3,
+    charcoal_4,
+    charcoal_5,
+    charcoal_6,
+    charcoal_7,
+    charcoal_8,
+  ],
+  commission: [
+    commission_1,
+    commission_2,
+    commission_3,
+    commission_4,
+    commission_5,
+  ],
+  exploration: [
+    exploration_1,
+    exploration_2,
+    exploration_3,
+    exploration_4,
+    exploration_5,
+    exploration_6,
+    exploration_7,
+    exploration_8,
+    exploration_9,
+    exploration_10,
+    exploration_11,
+    exploration_12,
+    exploration_13,
+    exploration_14,
+    exploration_15,
+    exploration_16,
+    exploration_17,
+    exploration_18,
+    exploration_19,
+    exploration_20,
+  ],
+};
